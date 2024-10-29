e9352de490764e87157517c5a02e6564b3d4d95f
pabloemanuelpbl <pablo.fju.pe@gmail.com>
2024-10-29 10:22:03 -0300

    feat: creation of the user entity and its versioning in alembic

--- src/sqlalchemy/entities/User.py
+++ src/sqlalchemy/entities/User.py
@@ -1,7 +1,12 @@
-from sqlalchemy import  Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from src.sqlalchemy.config.database import Base
 
-class Test(Base):
-    __tablename__ = "testes"
-    id = Column("id", Integer, primary_key=True, autoincrement=True)
-    name = Column("name", String)
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    username = Column(String(50), index=True, nullable=False)
+    email = Column(String(80), unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    is_admin = Column(Boolean, default=False)
